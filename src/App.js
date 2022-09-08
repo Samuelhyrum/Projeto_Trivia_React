d33e89d79ed32b084e69d4cfbc7e5f91907562f8
Renan Fernandes <renzinestuods@gmail.com>
2022-09-08 16:21:23 -0300

    Cria a rota para a pagina GameScreen

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import GameScreen from './pages/GameScreen';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/GameScreen" component={ GameScreen } />
     </Switch>
   );
 }
