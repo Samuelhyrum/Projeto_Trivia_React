ea0b7ea7cd4d0cb2c0b443acd6a07b32580eb5fe
Renan Fernandes <renzinestuods@gmail.com>
2022-09-09 20:00:50 -0300

    renderiza o componente Header

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
-      <div>Feedback</div>
+      <div>
+        <Header />
+        Feedback
+      </div>
     );
   }
 }
