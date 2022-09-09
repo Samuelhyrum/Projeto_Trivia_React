a482e32cc9a129a618c66a497659ea3e35d1bcb6
Renan Fernandes <renzinestuods@gmail.com>
2022-09-09 20:35:08 -0300

    REQUISITO-13: Cria a mensagem de feedback para ser exibida a pessoa usuária

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default class Feedback extends Component {
+const assertionsNumber = 3;
+class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
     return (
       <div>
         <Header />
-        <div data-testid="feedback-text">
-          Feedback
+        <div>
+          { assertions < assertionsNumber && (
+            <p data-testid="feedback-text">Could be better...</p>
+          )}
+          { assertions >= assertionsNumber && (
+            <p data-testid="feedback-text">Well Done!</p>
+          )}
         </div>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  score: propTypes.number,
+  assertions: propTypes.number,
+}.isRequired;
+
+const mapStateToProps = ({ player }) => ({
+  ...player,
+});
+
+export default connect(mapStateToProps)(Feedback);
