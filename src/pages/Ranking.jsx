7b762bd9f0c534a75b676cee4b160486cc8ea5fc
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-09-12 16:46:22 +0200

    requisito 18 concluido

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
       <div>
         <div data-testid="ranking-title">Ranking</div>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            Home
+          </button>
+        </Link>
       </div>
     );
   }
